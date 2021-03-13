f40735de508837d9ede7cadc9e5f9dac7a9a4621
Phern17 <mphern17@gmail.com>
2021-03-13 14:18:59 +0800

    End Loop after user guessed all 50 states

--- us-states-game-start/main.py
+++ us-states-game-start/main.py
@@ -10,8 +10,7 @@ turtle.shape(img)
 answer_manager = AnswerManager()
 correct_counter = 0
 
-game_is_on = True
-while game_is_on:
+while correct_counter < 50:
     user_input = screen.textinput(title=f"{correct_counter}/50 States Correct", prompt="What's the next state's name?").title()
     if answer_manager.check_states(user_input):
         answer_manager.plot_answer(user_input)
