7802a535ff81c3a7e8ea03a6dc0a79d6d56e8493
Phern17 <mphern17@gmail.com>
2021-03-13 14:36:09 +0800

    Added Export to csv function, User can type 'Exit' to quit the game, then have a CSV file of the remaining states that weren't guessed right.

--- us-states-game-start/answer_manager.py
+++ us-states-game-start/answer_manager.py
@@ -11,9 +11,17 @@ class AnswerManager(Turtle):
         self.all_states = self.data['state'].to_list()
 
     def check_states(self, user_ans):
-        return user_ans in self.all_states
+        if user_ans in self.all_states:
+            self.all_states.remove(user_ans)
+            return True
+        return False
 
     def plot_answer(self, destination):
         des_info = self.data[self.data['state'] == destination]
         self.goto(int(des_info.x), int(des_info.y))
         self.write(arg=f'{destination}', align='left')
+
+    def export_csv(self):
+        data_dict = {"State": self.all_states}
+        output = pandas.DataFrame(data_dict)
+        output.to_csv('remaining states.csv')
--- us-states-game-start/main.py
+++ us-states-game-start/main.py
@@ -11,9 +11,16 @@ answer_manager = AnswerManager()
 correct_counter = 0
 
 while correct_counter < 50:
-    user_input = screen.textinput(title=f"{correct_counter}/50 States Correct", prompt="What's the next state's name?").title()
+    user_input = screen.textinput(title=f"{correct_counter}/50 States Correct",
+                                  prompt="What's the next state's name?").title()
+
+    if user_input == "Exit":
+        answer_manager.export_csv()
+        break
+
     if answer_manager.check_states(user_input):
+
         answer_manager.plot_answer(user_input)
         correct_counter += 1
 
-turtle.mainloop()
+
